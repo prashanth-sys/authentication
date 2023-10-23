ff5ed046c56d3fedfb9fa294db5ffb3c33b6bddc
PrashanthKumaRerddy <reddyprashanth512@gmail.com>
2023-10-23 05:13:05 +0000

    solution

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+const bcrypt = require("bcrypt");
+
+const dbPath = path.join(__dirname, "userData.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+//API 1 POST
+
+app.post("/register", async (request, response) => {
+  let { username, name, password, gender, location } = request.body;
+  let hashedPassword = await bcrypt.hash(password, 10);
+  let checkTheUserName = `
+    SELECT 
+    *
+    FROM
+    user
+    WHERE 
+    username = ${username};`;
+  let userData = await db.get(checkTheUserName);
+  if (userData === undefined) {
+    let postNewUserQuery = `
+            INSERT INTO
+            user (username,name,password,gender,location)
+            VALUES (
+                '${username}',
+                '${name}',
+                '${hashedPassword}',
+                '${gender}',
+                '${location}'
+            );`;
+    if (password.length < 5) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      let newUserDetails = await db.run(postNewUserQuery);
+      response.status(200);
+      response.send("User created successfully");
+    }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
